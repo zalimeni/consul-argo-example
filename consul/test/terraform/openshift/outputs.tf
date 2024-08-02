9557153ec752a0f2ce7de4c147d30673fb94a653
Michael Zalimeni <michael.zalimeni@hashicorp.com>
2024-08-02 08:41:43 -0400

    move chart to top level

--- consul/test/terraform/openshift/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "kubeconfigs" {
-  value = [for rg in azurerm_resource_group.test : format("$HOME/.kube/%s", rg.name)]
-}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/hashicorp/consul-k8s/charts
-
-go 1.20
